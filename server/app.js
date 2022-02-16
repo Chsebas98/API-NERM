36289214d07507b64950c3baa941ae7efd87eaa6
Chsebas98 <chsebas98@gmail.com>
2022-02-16 12:31:19 -0500

    Start API

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+//db
+
+//middlewares
+
+//routes
+
+//listener
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+	console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
